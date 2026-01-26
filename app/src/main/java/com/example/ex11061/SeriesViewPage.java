54fe49ab31cdb518e62ef414620f08909a15f149
sh7478 <sh7478@bs.amalnet.k12.il>
2026-01-26 11:26:01 +0200

    Added series view page design

---
+++ app/src/main/java/com/example/ex11061/SeriesViewPage.java
@@ -0,0 +1,18 @@
+package com.example.ex11061;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SeriesViewPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series_view_page);
+    }
+}
